77f1783746cd27c83a26d25b07c138ed9ab4c286
Leo Sargsyan <leo.sargsyan73@gmail.com>
2025-02-12 20:59:10 +0400

    Added new changes in app.js and added new file test.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const getPosts = async () => {
+        try{
+                const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+                console.log(response.data);
+        } catch (error) {
+                console.error("There was an error making the GET request", error);
+        }
+};
+
+getPosts();
