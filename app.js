7c362a1bd9150e5e4cba3ca09d0bff0ee5366a20
Davit <davkhachatryan2405@gmail.com>
2025-02-16 01:40:18 +0400

    addd

---
+++ app.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+
+const getPosts = async () => {
+        try{
+                const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+                console.log(response.data);
+        } catch (error) {
+                console.error("There was an error making the GET request", error);
+        }
+};
+
+getPosts();
+console.log(1)
+console.log("bbb2")
+
+
