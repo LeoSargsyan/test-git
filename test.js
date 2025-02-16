bf665ab9a15481762e8c96428325a5e4c7d6d1ce
Leo Sargsyan <leo.sargsyan73@gmail.com>
2025-02-17 00:54:27 +0400

    changed logs

--- test.js
+++ test.js
@@ -3,11 +3,7 @@
 console.log("")
 console.log("")
 console.log("nnnn")
-console.log("mmmm")
-console.log("aa")
-console.log("hello")
-console.log("hi")
-console.log("by")
+
 import express from "express";
 const app = express();
 
