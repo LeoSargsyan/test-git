dac11fb90a9232abebb4a76d88b8bcff204ade8f
Davit <davkhachatryan2405@gmail.com>
2025-02-17 19:51:09 +0400

    changed app.js

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ import axios from "axios";
 
 
 const getPosts = async () => {
-        try{
-                const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
-                console.log(response.data);
-        } catch (error) {
-                console.error("There was an error making the GET request", error);
-        }
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+        console.log(response.data);
+    } catch (error) {
+        console.error("There was an error making the GET request", error);
+    }
 };
 
 getPosts();
 console.log(1)
-console.log("bbb2")
+console.log("bbb45")
 
-console.log("hiiii")
+console.log("ttt")
+console.log("aaaa2")
+console.log("bbb2")
